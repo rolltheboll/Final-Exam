c020549544b1753063e672913b63955a34edca37
rolltheboll <ralphserranoastete@gmail.com>
2025-02-16 17:17:12 -0800

    updated commit

--- main.py
+++ main.py
@@ -48,11 +48,17 @@ def get_users():
 def delete_user(id):
     user = User.query.get(id)
     if not user:
-     return jsonify({"message": "User not found"}), 404
+             return jsonify({"message": "User not found"}), 404
     db.session.delete(user)
     db.session.commit()
     return jsonify({"message": "User deleted"}), 204
 
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+          return jsonify({"message": "User not found"}), 404
+    return jsonify({"id": user.id, "name": user.name, "email": user.email}), 200
 
 @app.route('/books', methods=['POST'])
 def add_book():
@@ -66,6 +72,21 @@ def add_book():
 def get_books():
     books = Book.query.all()
     return jsonify([{ "id": b.id, "title": b.title, "author_id": b.author_id } for b in books]), 200
+
+@app.route('/books/author/<int:author_id>', methods=['GET'])
+def get_books_by_author(author_id):
+    books = Book.query.filter_by(author_id=author_id).all()
+    return jsonify([{"id": b.id, "title": b.title, "author_id": b.author_id} for b in books]), 200
+
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if not book:
+        return jsonify({"message": "Book not found"}), 404
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({"message": "Book deleted"}), 204
 
 
 @app.route('/authors', methods=['POST'])
@@ -81,6 +102,15 @@ def get_authors():
     authors = Author.query.all()
     return jsonify([{ "id": a.id, "name": a.name } for a in authors]), 200
 
+@app.route('/authors/<int:id>/books', methods=['GET'])
+def get_author_books(id):
+    author = Author.query.get(id)
+    if not author:
+       return jsonify({"message": "Author not found"}), 404
+    books = Book.query.filter_by(author_id=id).all()
+    return jsonify([{"id": b.id, "title": b.title} for b in books]), 200
+
+
 
 @app.route('/borrow', methods=['POST'])
 def borrow_book():
@@ -89,6 +119,25 @@ def borrow_book():
     db.session.add(new_borrow)
     db.session.commit()
     return jsonify({"message": "Book borrowed", "id": new_borrow.id}), 201
+
+@app.route('/users/<int:id>/borrowed_books', methods=['GET'])
+def get_borrowed_books(id):
+    user = User.query.get(id)
+    if not user:
+          return jsonify({"message": "User not found"}), 404
+    borrowed_books = Borrow.query.filter_by(user_id=id).all()
+    return jsonify([{"book_id": b.book_id, "borrow_date": b.borrow_date} for b in borrowed_books]), 200
+
+
+@app.route('/books/<int:id>/borrowers', methods=['GET'])
+def get_book_borrowers(id):
+    book = Book.query.get(id)
+    if not book:
+         return jsonify({"message": "Book not found"}), 404
+    borrowers = Borrow.query.filter_by(book_id=id).all()
+    return jsonify([{"user_id": b.user_id, "borrow_date": b.borrow_date} for b in borrowers]), 200
+
+
 
 if __name__ == '__main__':
     with app.app_context():
